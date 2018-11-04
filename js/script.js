f4696c80043f59ff8a3961ddc6e8ad98c64d743f
Ayush Sharma <contact.ayush95@gmail.com>
2018-11-03 18:10:57 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
 var app = document.getElementById('app');
-
 var typewriter = new Typewriter(app, {
     loop: true
 });
-
-typewriter.typeString('websites.')
-    .pauseFor(1500)
+var toggle_nav = function(x) {
+  nav = document.getElementById("side-nav");
+  button = document.getElementById("toggle-nav");
+  if(nav.style.width == '40%') {
+    $('.menu-items').fadeOut('fast');
+    nav.style.width = 0;
+    button.classList.toggle('animate');
+  }
+  else {
+    nav.style.width = '40%';
+    $('.menu-items').fadeIn();
+    button.classList.toggle('animate');
+  }
+}
+typewriter.typeString('websites.')	
+    .pauseFor(500)
     .deleteAll()
     .typeString('interfaces.')
-    .pauseFor(1500)
+    .pauseFor(500)
     .deleteAll()
     .typeString('experiences.')
-    .pauseFor(1500)
+    .pauseFor(500)
     .start();
+
 var navigate = function(e) {
 		var currentAttrValue = $(this).attr('href');		
 		// Show/Hide Tabs
-		$(currentAttrValue).fadeIn().siblings().hide();
+
+		$(currentAttrValue).addClass('active').fadeIn().siblings().hide().removeClass('active');
 		// // Change/remove current tab to active
 		$(this).addClass('highlighted').siblings().removeClass('highlighted');
 		e.preventDefault();
 	}
-var work_hl = function(e) {
+var redirect = function(e) {
 	var currentAttrValue = $(this).attr('href');
+	$(currentAttrValue).addClass('active').fadeIn().siblings().hide().removeClass('active');
+	$('a[href="' + currentAttrValue + '"]').addClass('highlighted').siblings().removeClass('highlighted');
 }
 $(document).ready(function() {
+	$('#home').siblings().hide();
+	$("#learn-more").on('click', redirect)
 	$('.menu a').on('click', navigate);
-
+	$('#work_link').on('click', redirect);
 });
